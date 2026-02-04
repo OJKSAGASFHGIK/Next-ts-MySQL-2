383fc17ad58561324b54f795a5c47f8e2cd579e5
Greque <ichijou_raku@hotmail.com>
2026-02-04 17:49:14 -0300

    Connecting Front to Back end.

--- next-ts-mysql/app/page.tsx
+++ next-ts-mysql/app/page.tsx
@@ -5,13 +5,19 @@ import Layout from "./pages/layout/page";
 import axios from "axios";
 
 export default function Home() {
-  const [books, setBooks] = useState([]);
+  type books = {
+    id: number;
+    title: string;
+    desc: string;
+    cover: string;
+  }
+  const [books, setBooks] = useState<books[]>([]);
 
   useEffect(() => {
     const fetchBooks = async() => {
       try {
         const res = await axios.get("http://localhost:8800/books");
-        console.log(res.data);
+        setBooks(res.data);
       } catch(err){
         console.log(err);
       }
@@ -22,7 +28,12 @@ export default function Home() {
 
   return (
       <Layout>
-        hmm
+        {books.map((book) => (
+          <div key={book.id}>
+            {book.cover && <img src={book.cover} alt="" />}
+            <h2>{book.title}</h2>
+          </div>
+        ))}
       </Layout>
     );
 }
--- next-ts-mysql/app/pages/layout/navbar/page.tsx
+++ next-ts-mysql/app/pages/layout/navbar/page.tsx
@@ -1,3 +1,3 @@
 export default function navbar(){
-    return(<div className="bg-red-500">navbar</div>)
+    return(<div className="bg-red-500">Greque Bookshop</div>)
 }
